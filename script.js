9619b90278995e2f8b3f8643fe7eb125d56f0f09
Joseph Dorman <JDorman555@yahoo.com>
2023-02-27 19:48:11 -0600

    Finish makeGrid, add user prompt for gridValue

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let gridValue = 16;
+let gridValue = prompt("Enter grid size");
 let gridSize = (760 / gridValue) - 2;
 console.log(gridSize);
 
@@ -8,11 +8,14 @@ function makeGrid(gridSize) {
 const container = document.querySelector('.container');
 
 for (let i = 0; i < gridValue; i++) {
-  const grid = document.createElement('div');
-  grid.classList.add('gridBox');
-  grid.setAttribute('style', `border: 1px solid black; width: ${gridSize + 'px'}; height: ${gridSize + 'px'}`);
+  for (let h = 0; h < gridValue; h++) {
+    const grid = document.createElement('div');
+    grid.classList.add('gridBox');
+    grid.setAttribute('style', `border: 1px solid black; width: ${gridSize + 'px'}; height: ${gridSize + 'px'}`);
 
-  container.appendChild(grid)
+    container.appendChild(grid)
+  }
 }
+
 
 }
