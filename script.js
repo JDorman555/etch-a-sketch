806ad3e6721695b5092f6c02006c02858fe5ea20
Joseph Dorman <JDorman555@yahoo.com>
2023-02-27 20:25:19 -0600

    Add hover effect

--- script.js
+++ script.js
@@ -5,17 +5,33 @@ console.log(gridSize);
 makeGrid(gridSize);
 
 function makeGrid(gridSize) {
-const container = document.querySelector('.container');
+  const container = document.querySelector('.container');
 
-for (let i = 0; i < gridValue; i++) {
-  for (let h = 0; h < gridValue; h++) {
-    const grid = document.createElement('div');
-    grid.classList.add('gridBox');
-    grid.setAttribute('style', `border: 1px solid black; width: ${gridSize + 'px'}; height: ${gridSize + 'px'}`);
+  for (let i = 0; i < gridValue; i++) {
+    for (let h = 0; h < gridValue; h++) {
+      const grid = document.createElement('div');
+      grid.classList.add('gridBox');
+      grid.setAttribute('style', `border: 1px solid black; width: ${gridSize + 'px'}; height: ${gridSize + 'px'}`);
+      grid.addEventListener('mouseover', () => {
+        grid.style.backgroundColor = "black";
+      });
 
-    container.appendChild(grid)
+      container.appendChild(grid)
+    }
   }
-}
 
 
 }
+
+/*
+const gridBoxs = document.querySelector('.gridBox');
+//gridBoxs.addEventListener("mouseover" , (gridColor))
+
+gridBoxs.forEach((gridBox) => {
+  gridBox.addEventListener('mouseover', gridColor);
+})
+
+function gridColor() {
+  grid.style.backgroundColor = "black";
+}
+*/
